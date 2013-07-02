6149164014306361910e1f41110a503f61071c65
moandji.ezana <mwanji@gmail.com>
2013-07-02 15:05:47 +0200

    Improved use of generics in HumptyBootstrap

--- src/main/java/co/mewf/humpty/config/HumptyBootstrap.java
+++ src/main/java/co/mewf/humpty/config/HumptyBootstrap.java
@@ -4,8 +4,10 @@ import co.mewf.humpty.Pipeline;
 import co.mewf.humpty.PostProcessor;
 import co.mewf.humpty.PreProcessor;
 import co.mewf.humpty.Resolver;
+import co.mewf.humpty.config.gson.GsonClassAdapter;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import java.io.InputStreamReader;
 import java.util.ArrayList;
@@ -13,7 +15,7 @@ import java.util.List;
 import java.util.ServiceLoader;
 
 /**
- * Uses humpty.json at root of classpath to load the Configuration.
+ * Uses humpty.json at root of classpath to load the Configuration (eg. src/main/resources in a Maven project).
  *
  * Uses a {@link ServiceLoader} to get the {@link Resolver}s, {@link PreProcessor}s and {@link PostProcessor}s.
  *
@@ -21,13 +23,13 @@ import java.util.ServiceLoader;
  */
 public class HumptyBootstrap {
 
-  private final Gson gson = new Gson();
+  private final Gson gson = new GsonBuilder().registerTypeAdapter(Class.class, new GsonClassAdapter()).create();
 
   public Pipeline createPipeline() {
     Configuration configuration = getConfiguration();
-    List<Resolver> resolvers = getResolvers();
-    List<PreProcessor> preProcessors = getPreProcessors();
-    List<PostProcessor> postProcessors = getPostProcessors();
+    List<? extends Resolver> resolvers = getResolvers();
+    List<? extends PreProcessor> preProcessors = getPreProcessors();
+    List<? extends PostProcessor> postProcessors = getPostProcessors();
 
     return new Pipeline(configuration, resolvers, preProcessors, postProcessors);
   }
@@ -36,7 +38,7 @@ public class HumptyBootstrap {
     return gson.fromJson(new InputStreamReader(getClass().getResourceAsStream("/humpty.json")), Configuration.class);
   }
 
-  protected List<Resolver> getResolvers() {
+  protected List<? extends Resolver> getResolvers() {
     ArrayList<Resolver> resolvers = new ArrayList<Resolver>();
     ServiceLoader<Resolver> serviceLoader = ServiceLoader.load(Resolver.class);
     for (Resolver resolver : serviceLoader) {
@@ -46,7 +48,7 @@ public class HumptyBootstrap {
     return resolvers;
   }
 
-  protected List<PreProcessor> getPreProcessors() {
+  protected List<? extends PreProcessor> getPreProcessors() {
     ArrayList<PreProcessor> preProcessors = new ArrayList<PreProcessor>();
     ServiceLoader<PreProcessor> serviceLoader = ServiceLoader.load(PreProcessor.class);
     for (PreProcessor preProcessor : serviceLoader) {
@@ -56,7 +58,7 @@ public class HumptyBootstrap {
     return preProcessors;
   }
 
-  protected List<PostProcessor> getPostProcessors() {
+  protected List<? extends PostProcessor> getPostProcessors() {
     ArrayList<PostProcessor> postProcessors = new ArrayList<PostProcessor>();
     ServiceLoader<PostProcessor> serviceLoader = ServiceLoader.load(PostProcessor.class);
     for (PostProcessor postProcessor : serviceLoader) {
