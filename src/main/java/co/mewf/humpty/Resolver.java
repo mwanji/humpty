22fc0f1dde66751248922a0f92b3276d6f3e50ff
moandji.ezana <mwanji@gmail.com>
2013-07-03 23:45:52 +0200

    Added JavaDoc to Resolver

--- src/main/java/co/mewf/humpty/Resolver.java
+++ src/main/java/co/mewf/humpty/Resolver.java
@@ -3,7 +3,13 @@ package co.mewf.humpty;
 import co.mewf.humpty.config.Context;
 
 import java.io.Reader;
+import java.util.ServiceLoader;
 
+/**
+ * Uses a URI to locate an asset.
+ *
+ * Resolvers should be declared in META-INF/services/co.mewf.humpty.Resolver so they can be picked up by a {@link ServiceLoader}
+ */
 public interface Resolver {
 
   boolean accepts(String uri);
