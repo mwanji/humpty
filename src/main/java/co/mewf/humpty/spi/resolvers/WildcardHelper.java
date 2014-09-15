346acd8db88193cbe4f6ff3a8127bce023db38d5
moandji.ezana <mwanji@gmail.com>
2014-09-15 08:07:54 +0200

    Removed some unused code from WildcardHelper

--- src/main/java/co/mewf/humpty/spi/resolvers/WildcardHelper.java
+++ src/main/java/co/mewf/humpty/spi/resolvers/WildcardHelper.java
@@ -1,19 +1,15 @@
 package co.mewf.humpty.spi.resolvers;
 
-import co.mewf.humpty.config.Context;
-
 import org.apache.commons.io.FilenameUtils;
 
 class WildcardHelper {
 
   private final String uri;
-  private final Context context;
   private final String extension;
 
-  WildcardHelper(String uri, Context context) {
+  WildcardHelper(String uri) {
     this.uri = uri;
-    this.context = context;
-    this.extension = FilenameUtils.getExtension(uri).isEmpty() ? FilenameUtils.getExtension(uri) : FilenameUtils.getExtension(context.getBundleName());
+    this.extension = FilenameUtils.getExtension(uri);
   }
 
   public boolean hasWildcard() {
@@ -33,7 +29,7 @@ class WildcardHelper {
   }
 
   /**
-   * @return true if fileName is acceptable within the context of this uri and bundle
+   * @return true if fileName is acceptable within the context of this uri
    */
   public boolean matches(String fileName) {
     return FilenameUtils.isExtension(fileName, extension);
