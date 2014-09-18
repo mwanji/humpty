d411fed96a02df87d26b7b033d40bf711ff4fac0
moandji.ezana <mwanji@gmail.com>
2014-09-18 13:05:08 +0200

    Updated to parent 0.4.0 and replaced Rhino with Nashorn

--- src/test/java/co/mewf/humpty/spi/processors/CoffeeScriptSourceProcessor.java
+++ src/test/java/co/mewf/humpty/spi/processors/CoffeeScriptSourceProcessor.java
@@ -1,13 +1,13 @@
 package co.mewf.humpty.spi.processors;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.Reader;
+import java.util.HashMap;
 
-import org.apache.commons.io.IOUtils;
-import org.mozilla.javascript.Context;
-import org.mozilla.javascript.Scriptable;
+import javax.script.Invocable;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+
 import org.webjars.WebJarAssetLocator;
 
 import co.mewf.humpty.config.PreProcessorContext;
@@ -28,27 +28,20 @@ public class CoffeeScriptSourceProcessor implements SourceProcessor {
 
   @Override
   public CompilationResult compile(SourceProcessor.CompilationResult compilationResult, PreProcessorContext context) {
+    ScriptEngine nashorn = new ScriptEngineManager().getEngineByName("nashorn");
+
     ClassLoader classLoader = getClass().getClassLoader();
     InputStream inputStream = classLoader.getResourceAsStream(COFFEE_SCRIPT_JS);
-    try {
-      Reader csReader = new InputStreamReader(inputStream, "UTF-8");
-      Context rhinoContext = Context.enter();
-      Scriptable globalScope = rhinoContext.initStandardObjects();
-      rhinoContext.evaluateReader(globalScope, csReader, "coffee-script.js", 0, null);
-
-      Scriptable compileScope = rhinoContext.newObject(globalScope);
-      compileScope.setParentScope(globalScope);
-      compileScope.put("coffeeScriptSource", compileScope, compilationResult.getAsset());
-
-      String compiled = (String) rhinoContext.evaluateString(compileScope,
-          "CoffeeScript.compile(coffeeScriptSource, { bare: true });", "JCoffeeScriptCompiler", 0, null);
+    try (InputStreamReader coffeeScriptReader = new InputStreamReader(inputStream, "UTF-8")) {
+      nashorn.eval(coffeeScriptReader);
+      Object coffeeScript = nashorn.eval("CoffeeScript");
+      HashMap<String, Object> options = new HashMap<String, Object>();
+      options.put("bare", true);
+      String compiled = (String) ((Invocable) nashorn).invokeMethod(coffeeScript, "compile", compilationResult.getAsset(), options);
 
       return new CompilationResult(compilationResult.getAssetName().replace(".coffee", ".js"), compiled);
-    } catch (IOException e) {
+    } catch (Exception e) {
       throw new RuntimeException(e);
-    } finally {
-      IOUtils.closeQuietly(inputStream);
-      Context.exit();
     }
   }
 }
