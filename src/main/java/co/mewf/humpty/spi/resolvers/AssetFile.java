107d00f6e7823c09cec5704d07f579198892f42f
moandji.ezana <mwanji@gmail.com>
2014-10-22 11:22:49 +0200

    Removed unused AssetFile methods

--- src/main/java/co/mewf/humpty/spi/resolvers/AssetFile.java
+++ src/main/java/co/mewf/humpty/spi/resolvers/AssetFile.java
@@ -1,46 +1,25 @@
 package co.mewf.humpty.spi.resolvers;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-
 import co.mewf.humpty.config.Bundle;
 
 public class AssetFile {
 
   private final String path;
-  private final File file;
   private final String contents;
   private final Bundle bundle;
-
-  public AssetFile(Bundle bundle, String path, File file) {
-    this.bundle = bundle;
-    this.path = path;
-    this.file = file;
-    this.contents = null;
-  }
 
   public AssetFile(Bundle bundle, String path, String contents) {
     this.bundle = bundle;
     this.path = path;
     this.contents = contents;
-    this.file = null;
   }
 
   public String getPath() {
     return path;
   }
 
-  public File getFile() {
-    return file;
-  }
-
   public String getContents() {
-    try {
-      return file != null ? new String(Files.readAllBytes(file.toPath())) : contents;
-    } catch (IOException e) {
-      throw new RuntimeException(e);
-    }
+    return contents;
   }
 
   public Bundle getBundle() {
