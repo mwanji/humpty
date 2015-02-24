de51bf01ba0400944433f25baa1425c016fed9e0
moandji.ezana <mwanji@gmail.com>
2015-02-24 14:08:54 +0200

    Digester fingerprints with MD5

--- src/main/java/co/mewf/humpty/tools/Digester.java
+++ src/main/java/co/mewf/humpty/tools/Digester.java
@@ -9,7 +9,10 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.List;
+import java.util.stream.IntStream;
 import java.util.zip.GZIPOutputStream;
 
 import javax.inject.Inject;
@@ -21,6 +24,9 @@ import co.mewf.humpty.config.Configuration;
 import com.moandjiezana.toml.Toml;
 
 public class Digester {
+  
+  private static final char[] HEX_CHARS = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+
   
   public Toml processBundles(Pipeline pipeline, List<Bundle> bundles, Path buildDir, Path humptyDigestToml) {
     if (humptyDigestToml.toFile().exists()) {
@@ -38,7 +44,7 @@ public class Digester {
       String bundleName = b.getName();
       String asset = pipeline.process(bundleName).getAsset();
       
-      Path bundleDigestPath = buildDir.resolve(getFullDigestName(bundleName, asset.hashCode()));
+      Path bundleDigestPath = buildDir.resolve(getFullDigestName(bundleName, asset));
       Path bundlePathGzip = buildDir.resolve(bundleDigestPath.getFileName() + ".gz");
       try {
         Files.write(bundleDigestPath, asset.getBytes(UTF_8));
@@ -69,7 +75,26 @@ public class Digester {
 //    }
   }
   
-  private String getFullDigestName(String name, int digest) {
-    return name.substring(0, name.lastIndexOf('.')) + "-humpty" + digest + name.substring(name.lastIndexOf('.'));
+  private String getFullDigestName(String name, String asset) {
+    try {
+      MessageDigest messageDigest = MessageDigest.getInstance("MD5");
+      String fingerprint = encodeHex(messageDigest.digest(asset.getBytes()));
+      
+      return name.substring(0, name.lastIndexOf('.')) + "-humpty" + fingerprint + name.substring(name.lastIndexOf('.'));
+    } catch (NoSuchAlgorithmException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  // Adapted from org.springframework.util.DigestUtils
+  private static String encodeHex(byte[] bytes) {
+    StringBuilder sb = new StringBuilder();
+    IntStream.range(0, 32).forEach(i -> {
+      byte b = bytes[i / 2];
+      sb.append(HEX_CHARS[(b >>> 0x4) & 0xf])
+        .append(HEX_CHARS[b & 0xf]);
+    });
+
+    return sb.toString();
   }
 }
